d3f6bd6f8de312f235a2df5b807418d1a2ae0d15
Serhii Tyrinov <liebhaben1981@gmail.com>
2025-06-12 21:24:30 +0300

    Added all files

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,40 @@
-output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.cdn.domain_name
+output "data_aws_availability_zones" {
+  value = data.aws_availability_zones.availability_zones.names
 }
 
-output "s3_bucket_name" {
-  value = aws_s3_bucket.website_bucket.id
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "data_aws_region_name" {
+  value = data.aws_region.current.name
+}
+
+output "data_aws_region_description" {
+  value = data.aws_region.current.description
+}
+
+output "data_aws_vpcs_my_vpcs" {
+  value = data.aws_vpcs.my_vpcs.ids
+}
+
+output "data_aws_route53_zone_id" {
+  value = data.aws_route53_zone.styrinov.zone_id
+}
+
+output "latest_ubuntu_ami_id" {
+  value = data.aws_ami.latest_ubuntu.id
+}
+
+output "latest_ubuntu_ami_name" {
+  value = data.aws_ami.latest_ubuntu.name
+}
+
+output "eip_public_ip" {
+  value = aws_eip.main.public_ip
+}
+
+output "ec2_public_ip" {
+  value       = aws_instance.my_webserver.public_ip
+  description = "Public IP of the EC2 instance"
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,23 @@
-variable "bucket_name" {
-  description = "Unique S3 bucket name"
-  type        = string
-  default     = "95145adf-8ef7-4823-8351-e4ed2abcec3e-working-with-s3"
-}
-
 variable "region" {
   description = "AWS region"
   type        = string
   default     = "eu-north-1"
+}
+
+variable "my_domain" {
+  description = "Hosted zone name"
+  type        = string
+  default     = "styrinov.com"
+}
+
+variable "subdomain" {
+  type        = string
+  description = "ec2web"
+  default     = "ec2web"
+}
+
+variable "lord_of_terraform" {
+  description = "Owner of this project"
+  type        = string
+  default     = "Serhii Tyrinov"
 }
