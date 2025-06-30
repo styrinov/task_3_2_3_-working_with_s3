09302497aa3b98e42ccdb623c4707272d21fb5b2
Serhii Tyrinov <liebhaben1981@gmail.com>
2025-06-30 17:01:06 +0300

    Pizdec

--- terraform.tf
+++ terraform.tf
@@ -11,6 +11,11 @@ terraform {
       source  = "hashicorp/null"
       version = "~> 3.2"
     }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
 
   backend "s3" {
